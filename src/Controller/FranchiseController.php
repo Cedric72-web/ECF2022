5e67be33e5278ee7ac59a00a4f20eb55e9b79e56
VESQUE Cédric <vesque.cedric@bbox.fr>
2022-10-31 20:51:47 +0100

    Add franchise controller

--- src/Controller/FranchiseController.php
+++ src/Controller/FranchiseController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Franchise;
+use App\Repository\PartnerRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FranchiseController extends AbstractController
 {
     #[Route('/franchise', name: 'franchise')]
-    public function index(): Response
+    public function index(Franchise $franchise, PartnerRepository $partnerRepo, ManagerRegistry $doctrine): Response
     {
+        $user = $this->getUser();
+        $franchise = $doctrine->getRepository(Franchise::class);
         return $this->render('franchise/index.html.twig', [
             'controller_name' => 'FranchiseController',
+            'user' => $user,
         ]);
     }
 }
